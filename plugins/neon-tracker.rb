9af23a80b66ab357ea4f5f8d5fd485a499ae384c
oscarsiniscalchi <oscar@e-nvy.com>
2012-09-28 10:02:08 -0300

    Added validation for project and username.WIP json parsing error

---
+++ plugins/neon-tracker.rb
@@ -0,0 +1,69 @@
+if ENV['TIMELOG']
+  require 'net/http'
+  require 'json'
+  timer = Ti::Timer
+
+  module Ti
+    class  << Timer
+
+      def post_time (user, pass, options = {})
+
+        puts "Uploading..."
+
+        postdata = {
+          project:         options[:project] || self.client,
+          description:     self.task,
+          minutes:      self.minutes,
+          user_auth_email: user,
+          user_auth_pass:  pass
+        }
+
+        uri = URI('http://neon-tracker.herokuapp.com/api/tracktime')
+        res = Net::HTTP.post_form(uri, postdata )
+      end
+
+    end
+  end
+
+  timer.on :finish do
+    puts "Do you want to submmit your working hours to NeonTracker now? | Y/n"
+    answer = STDIN.gets.chomp
+    if answer == 'Y'
+
+      puts "Enter your username:"
+      user = STDIN.gets.chomp
+      puts "Password: "
+      pass = STDIN.gets.chomp
+
+      errors = true
+      options = {}
+      while errors do
+        res = timer.post_time(user, pass, options)
+
+        p res
+        p res.body
+        p res.body.to_json
+        p res.body.to_json[0]
+        if res.body[:response] == "success"
+          puts res.body[:message].color(:green)
+          errors = false
+        else
+          errors = true
+          puts res.body[:message].color(:red)
+
+          case res.body[:error_type]
+          when "invalid_project"
+            puts "Choose one of this projects"
+            puts res.body[:options]
+            project = STDIN.gets.chomp
+            options[:project] = project
+
+          when "invalid_user"
+            puts "Invalid username or password, try again"
+          end
+        end
+      end
+
+    end
+  end
+end
